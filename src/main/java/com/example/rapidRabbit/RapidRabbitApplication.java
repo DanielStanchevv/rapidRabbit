c0036d83186ad747ec28654951d5663387067a04
Daniel Stanchev <danielstanchev@Daniels-MacBook-Pro.local>
2025-02-12 13:01:55 +0200

    Initial commit

---
+++ src/main/java/com/example/rapidRabbit/RapidRabbitApplication.java
@@ -0,0 +1,13 @@
+package com.example.rapidRabbit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RapidRabbitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RapidRabbitApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/rapidRabbit/RapidRabbitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.rapidRabbit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RapidRabbitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
